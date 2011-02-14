1515fef919e9269ca7c216afbded397f3a0d7804
Seyun Choi <npcomple@naver.com>
2011-02-13 20:20:14 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyLikes::Application.config.secret_token = '884cf20473d6c94c1e20b1e94afa8c4c6f864ae5a66b7579bc2f34aa2fc1ce781398666dbb0f947519763cd0c8bed084bf2e82bb0147aa5f60c824ed74b425a0'
